6041c8e593d7447de5102ff8856cd54b9aa268e2
Владислав Колокольчиков <v.kolokolchikov.work@inbox.ru>
2024-01-08 15:53:17 +0300

    fix(init-1): init project

--- front/vite-project/src/widgets/footerItems/footerNavBar.jsx
+++ front/vite-project/src/widgets/footerItems/footerNavBar.jsx
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom";
 const FooterNavBar = () => {
     return (
         <div className={"footer-nav-bar"}>
-            <Link to={"/about"} className={"footer-text"}> О нас </Link>
-            <Link to={"/delivery"} className={"footer-text"}> Доставка </Link>
-            <Link to={"/catalog"} className={"footer-text"}> Каталог </Link>
+            <Link to={"/about/"} className={"footer-text"}> О нас </Link>
+            <Link to={"/delivery/"} className={"footer-text"}> Доставка </Link>
+            <Link to={"/catalog/"} className={"footer-text"}> Каталог </Link>
             <Link to={"/contacts/"} className={"footer-text"}> Контакты </Link>
         </div>
     );
