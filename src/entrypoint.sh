4652e8482634f41fa6cc700f8c3646a1a2b3619d
Владислав Колокольчиков <v.kolokolchikov.work@inbox.ru>
2024-01-15 22:38:02 +0300

    fix(init-2): fix docker-config

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,5 +3,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
+python manage.py setup_notifications
 
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 4 --threads 4
