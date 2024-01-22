603e25554415707ff74b3c91cd4fc713136d768c
Владислав Колокольчиков <v.kolokolchikov.work@inbox.ru>
2024-01-22 23:28:35 +0300

    fix(refactoring-2): fix front

--- front/vite-project/src/features/furnitureDetail/index.jsx
+++ front/vite-project/src/features/furnitureDetail/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import BaseSlider from "../../shared/ui/baseSlider/index.jsx";
 import BaseTitle from "../../shared/ui/baseTitle/index.jsx";
+import HTMLContent from "../../shared/ui/baseHTMLParserContent/index.jsx";
 
 const FurnitureDetail = ({item}) => {
     return (
         <div>
             <BaseTitle text={item.title} />
-            <p> {item.describe}</p>
+            <div> <HTMLContent content={item.describe} /> </div>
             <BaseSlider items={item.images} section={"photo"} />
         </div>
     );
