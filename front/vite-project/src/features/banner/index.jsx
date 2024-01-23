31550342f9a3e637b61f9fac6f6a9c3145676e3a
Владислав Колокольчиков <v.kolokolchikov.work@inbox.ru>
2024-01-23 19:18:19 +0300

    fix(front-fix): fix front

--- front/vite-project/src/features/banner/index.jsx
+++ front/vite-project/src/features/banner/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from "../../assets/logo-on-banner.png"
+import title from "../../assets/title.png"
 import './style.scss'
 
 
@@ -9,6 +10,7 @@ const Banner = () => {
         <>
             <div className="banner-box">
                 <img className={"banner-box__img"} src={logo} alt={"logo"}/>
+                <img className={"banner-box__title"} src={title} alt={"title"}/>
                 <h1 className={"banner-box__title-describe"}> Мебель · для · жизни</h1>
             </div>
         </>
